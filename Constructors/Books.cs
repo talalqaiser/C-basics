a7ed9ce319dc840c697d200530bfee2bc0d312fa
talalqaiser <99319852+talalqaiser@users.noreply.github.com>
2023-04-23 15:39:03 +0200

    Add files via upload

---
+++ Constructors/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Books
+    {
+        public string title;
+        public string author;
+        public int year;
+        public int pages;
+        public Books(string atitle, string aauthor, int ayear, int apages) { 
+            title = atitle;
+            author = aauthor;
+            year = ayear;
+            pages = apages;
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Constructors;
+
+//Console.WriteLine("Hello, World!");
+
+namespace Practice {
+    class Constructors { 
+        static void Main(string[] args)
+        {
+            Books book1 = new Books("Harry Potter and the Philosopher's Stone", "J. K. Rowling", 1997, 223);
+            Books book2 = new Books("The Lord of the Rings", "J. R. R. Tolkien", 1954, 423);
+            Books book3 = new Books("The Forty Rules of Love", "Elif Shafak", 2009, 354);
+
+            Console.WriteLine(book3.title);
+            Console.ReadLine();
+        }
+    }
+}
